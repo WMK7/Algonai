528b743b5a773a0ee422dcf893f5a016142e8956
WMK7 <micaelmm36@gmail.com>
2024-12-04 21:38:37 -0400

    Implementado iteração entre não tenho conta com a tela criar conta e o já possuo conta com a tela login com o splash

--- app/src/main/java/com/example/algonai/CriarContaActivity.kt
+++ app/src/main/java/com/example/algonai/CriarContaActivity.kt
@@ -1,20 +1,25 @@
 package com.example.algonai
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.algonai.databinding.ActicityLoginBinding
+import com.example.algonai.databinding.ActivityCriarContaBinding
 
 class CriarContaActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCriarContaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_criar_conta)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityCriarContaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.idJaTenhoConta.setOnClickListener {
+            val intent = Intent(this, SplashActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/algonai/LoginActivity.kt
+++ app/src/main/java/com/example/algonai/LoginActivity.kt
@@ -1,20 +1,26 @@
 package com.example.algonai
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.algonai.databinding.ActicityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActicityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.acticity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActicityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Botão de login para criar uma conta
+        binding.idCriarConta.setOnClickListener {
+            val intent = Intent(this, CriarContaActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
